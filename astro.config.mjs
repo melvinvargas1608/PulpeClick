4d9640759d502a37c29de0f49c86fcee43063736
unevisahn <contactounevisa@gmail.com>
2026-07-27 15:07:05 -0600

    fix: switch to @astrojs/vercel adapter for serverless deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://profevende.vercel.app',
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: vercel(),
   integrations: [react()],
 
   vite: {
